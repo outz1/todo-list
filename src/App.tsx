2a7918c0c135c0a3fc37712b313bcdb5af97c4e2
Eduardo de Brito Soares <du.ebrits@gmail.com>
2025-06-02 16:10:34 -0300

    add text and icons components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import Text from "./components/text";
+import Icon from "./components/icons";
+import TrashIcon from "./assets/icons/thrash.svg?react";
+import CheckIcon from "./assets/icons/checked.svg?react"
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
 
 function App() {
-
   return (
-    <div className="text-pink-dark">
-    Olá, mundo!
+    <div>
+      <div className="flex flex-col gap-2">
+        <Text variant="body-md" className="text-pink-base">
+          {" "}
+          Olá Mundo!{" "}
+        </Text>
+        <Text variant="body-sm-bold" className="text-green-base">
+          {" "}
+          Olá Mundo!{" "}
+        </Text>
+        <Text variant="body-md-bold"> Olá Mundo! </Text>
+      </div>
+      <div>
+        <Icon svg={TrashIcon} className="fill-pink-base"/>
+        <Icon svg={CheckIcon} className="fill-pink-base"/>
+        <Icon svg={PencilIcon} className="fill-pink-base"/>
+        <Icon svg={PlusIcon} className="fill-pink-base"/>
+        <Icon svg={SpinnerIcon} className="fill-pink-base "/>
+        <Icon svg={XIcon} className="fill-pink-base"/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
